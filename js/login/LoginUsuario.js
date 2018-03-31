38e50b2fc4301ecaa07fce4083d2c18d5e45df64
kleber <kleber099@gmail.com>
2018-03-31 13:03:23 -0300

    Adicionando os cartoes ao localStorage

--- js/login/LoginUsuario.js
+++ js/login/LoginUsuario.js
@@ -1,16 +1,22 @@
 let logado = JSON.parse(localStorage.getItem("logado"))
+let usuario = localStorage.getItem("nomeUsuario")
+let login = new EventEmitter2()
 
 LoginUsuario_render({
     logado: logado
     ,usuario: localStorage.getItem("nomeUsuario")
     ,onLogin: (nomeUsuario) => {
         logado = true
+        console.log(nomeUsuario);
         localStorage.setItem("logado", true)
         localStorage.setItem("nomeUsuario", nomeUsuario)
+        usuario = nomeUsuario
+        login.emit("login")
     },
     onLogout: () => {
         logado = false
         localStorage.setItem("logado", false)
-        localStorage.removeItem("nomeUsuario", undefined)
+        localStorage.removeItem("nomeUsuario")
+        login.emit("logout")
     }
 })
